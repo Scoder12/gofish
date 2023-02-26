ce858b208da9f3076cbe24cdb1be2d726ed475a2
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-02-26 15:09:20 -0800

    svelte-forms

--- frontend-svelte/src/lib/Button.svelte
+++ frontend-svelte/src/lib/Button.svelte
@@ -1,11 +1,9 @@
-<script lang="ts">
-</script>
-
 <button
 	{...$$props}
 	class={'border border-neutral-800 rounded-sm bg-neutral-200 hover:bg-neutral-300 ' +
 		'disabled:bg-neutral-200 disabled:opacity-50 p-2 text-xl ' +
 		($$props.class ?? '')}
+	on:click
 >
 	<slot />
 </button>
--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -2,32 +2,45 @@
 	import Button from '../lib/Button.svelte';
 	import Center from '../lib/Center.svelte';
 
-	let name = '';
-	let pin: number | null = null;
-	let errors = { name: '' };
+	import { form, field } from 'svelte-forms';
+	import { required } from 'svelte-forms/validators';
+
+	const name = field('name', '', [required()]);
+	const pin = field('pin', '', []);
+	const myForm = form(name);
+
 	let isSubmitting = false;
+	let formDisabled = false;
+	$: formDisabled = !$myForm.valid || isSubmitting;
+
+	function submit() {
+		isSubmitting = true;
+	}
 </script>
 
 <Center>
 	<div class="block">
 		<div class="block">
 			<h3>Enter Name</h3>
-			<div class={'text-red-500 font-semibold ' + (errors.name ? '' : 'invisible')}>
-				{errors.name || 'A'}
+			<div
+				class={'text-red-500 font-semibold ' +
+					($myForm.hasError('name.required') ? 'invisible' : '')}
+			>
+				Name is required
 			</div>
-			<input type="text" name="name" class="text-3xl" size="15" value={name} />
+			<input type="text" name="name" class="text-3xl" size="15" bind:value={$name.value} />
 		</div>
 		<hr class="my-3" />
 		<div>
 			<h3>Existing Game</h3>
 			<div class="flex w-full">
 				<input type="number" name="pin" class="text-3xl mr-1 flex-grow" size="10" value={pin} />
-				<Button disabled={isSubmitting}>JOIN</Button>
+				<Button disabled={formDisabled} on:click={submit}>JOIN</Button>
 			</div>
 		</div>
 		<div>
 			<h3>New Game</h3>
-			<Button class="float-right" disabled={isSubmitting}>Create Game</Button>
+			<Button class="float-right" disabled={formDisabled} on:click={submit}>Create Game</Button>
 		</div>
 	</div>
 </Center>
