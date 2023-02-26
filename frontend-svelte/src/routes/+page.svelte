6757531f56de2f9ee08432968f51554f6fca4da3
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-02-26 15:23:52 -0800

    svelte-forms-lib

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -2,45 +2,63 @@
 	import Button from '../lib/Button.svelte';
 	import Center from '../lib/Center.svelte';
 
-	import { form, field } from 'svelte-forms';
-	import { required } from 'svelte-forms/validators';
+	import { createForm } from 'svelte-forms-lib';
 
-	const name = field('name', '', [required()]);
-	const pin = field('pin', '', []);
-	const myForm = form(name);
+	const { form, errors, state, handleChange, handleSubmit } = createForm({
+		initialValues: {
+			name: '',
+			pin: ''
+		},
+		validate: (values) => {
+			let errors: any = {};
+			if (values.name === '') {
+				errors.name = 'Name is required';
+			}
+			return errors;
+		},
+		onSubmit: (values) => {
+			alert(JSON.stringify(values));
+		}
+	});
 
-	let isSubmitting = false;
 	let formDisabled = false;
-	$: formDisabled = !$myForm.valid || isSubmitting;
-
-	function submit() {
-		isSubmitting = true;
-	}
+	function submit() {}
 </script>
 
 <Center>
-	<div class="block">
+	<form class="block" on:submit={handleSubmit}>
 		<div class="block">
 			<h3>Enter Name</h3>
-			<div
-				class={'text-red-500 font-semibold ' +
-					($myForm.hasError('name.required') ? 'invisible' : '')}
-			>
-				Name is required
+			<div class={'text-red-500 font-semibold ' + ($errors.name ? '' : 'invisible')}>
+				{$errors.name || 'A'}
 			</div>
-			<input type="text" name="name" class="text-3xl" size="15" bind:value={$name.value} />
+			<input
+				type="text"
+				name="name"
+				class="text-3xl"
+				size="15"
+				on:change={handleChange}
+				bind:value={$form.name}
+			/>
 		</div>
 		<hr class="my-3" />
 		<div>
 			<h3>Existing Game</h3>
 			<div class="flex w-full">
-				<input type="number" name="pin" class="text-3xl mr-1 flex-grow" size="10" value={pin} />
-				<Button disabled={formDisabled} on:click={submit}>JOIN</Button>
+				<input
+					type="number"
+					name="pin"
+					class="text-3xl mr-1 flex-grow"
+					size="10"
+					on:change={handleChange}
+					bind:value={$form.pin}
+				/>
+				<Button disabled={formDisabled} type="submit">JOIN</Button>
 			</div>
 		</div>
 		<div>
 			<h3>New Game</h3>
-			<Button class="float-right" disabled={formDisabled} on:click={submit}>Create Game</Button>
+			<Button class="float-right" disabled={formDisabled} type="submit">Create Game</Button>
 		</div>
-	</div>
+	</form>
 </Center>
