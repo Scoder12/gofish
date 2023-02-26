2a13bef95423c5cc631bb2456b79c94168e254fd
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-02-26 14:50:44 -0800

    noway

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '../lib/Button.svelte';
+	import Center from '../lib/Center.svelte';
+
+	let name = '';
+	let pin: number | null = null;
+	let errors = { name: '' };
+	let isSubmitting = false;
+</script>
+
+<Center>
+	<div class="block">
+		<div class="block">
+			<h3>Enter Name</h3>
+			<div class={'text-red-500 font-semibold ' + (errors.name ? '' : 'invisible')}>
+				{errors.name || 'A'}
+			</div>
+			<input type="text" name="name" class="text-3xl" size="15" value={name} />
+		</div>
+		<hr class="my-3" />
+		<div>
+			<h3>Existing Game</h3>
+			<div class="flex w-full">
+				<input type="number" name="pin" class="text-3xl mr-1 flex-grow" size="10" value={pin} />
+				<Button disabled={isSubmitting}>JOIN</Button>
+			</div>
+		</div>
+		<div>
+			<h3>New Game</h3>
+			<Button class="float-right" disabled={isSubmitting}>Create Game</Button>
+		</div>
+	</div>
+</Center>
