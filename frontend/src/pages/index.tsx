84198ca2c26e3eb04d7aa74831eda02ee144db5e
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-02-25 16:56:36 -0800

    basics

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,45 @@
+import { MutableRefObject, PropsWithChildren, useRef, useState } from "react";
+
+function Center({ children }: PropsWithChildren<{}>) {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      {children}
+    </div>
+  );
+}
+
+enum GameStateId {
+  Menu,
+  Lobby,
+}
+
+type GameState = { type: GameStateId.Menu } | { type: GameStateId.Lobby };
+
+type MaybeConnection = WebSocket | null;
+type ConnectionRef = MutableRefObject<MaybeConnection>;
+
+function Menu({ connection }: { connection: ConnectionRef }) {
+  return (
+    <Center>
+      <div className="block">
+        <div>
+          <h3>Join Game</h3>
+        </div>
+        <div>
+          <h3>Create Game</h3>
+        </div>
+      </div>
+    </Center>
+  );
+}
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [gameState, setGameState] = useState<GameState>({
+    type: GameStateId.Menu,
+  });
+  const connection: ConnectionRef = useRef<MaybeConnection>(null);
+
+  if (gameState.type == GameStateId.Menu) {
+    return <Menu connection={connection} />;
+  }
 }
