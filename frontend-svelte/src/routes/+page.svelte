803697c7dfeb9a4069ec9ef4e1e8bd98f4cffbc2
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-02-26 19:08:16 -0800

    initial server connection

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,16 +1,89 @@
 <script lang="ts">
 	import Center from '../lib/Center.svelte';
+	import * as flatbuffers from 'flatbuffers';
 
 	import Name from './Name.svelte';
 	import RoomType from './RoomType.svelte';
+	import {
+		Cmsg,
+		CmsgTable,
+		Create,
+		GameCreationResponseS,
+		GameRef,
+		IdentifyC,
+		Join,
+		SmsgTable
+	} from '../go-fish';
+	import { env } from '$env/dynamic/public';
 
 	enum Stage {
 		Name,
-		RoomType
+		RoomType,
+		Connecting
 	}
 
 	let stage = Stage.Name;
 	let name = '';
+
+	function gameFromDetails(fbb: flatbuffers.Builder, detail: any): [GameRef, number] {
+		if (detail.type == 'create') {
+			return [GameRef.Create, Create.createCreate(fbb)];
+		}
+		if (detail.type == 'join') {
+			return [GameRef.Join, Join.createJoin(fbb, detail.pin)];
+		}
+		throw new Error('Invalid game type');
+	}
+
+	function identifyMsg(detail: any): Uint8Array {
+		const fbb = new flatbuffers.Builder(1);
+		const nameOffset = fbb.createString(name);
+		const [gameType, gameOffset] = gameFromDetails(fbb, detail);
+		const identifyOffset = IdentifyC.createIdentifyC(fbb, nameOffset, gameType, gameOffset);
+		const msgTableOffset = CmsgTable.createCmsgTable(fbb, Cmsg.IdentifyC, identifyOffset);
+		fbb.finish(msgTableOffset, undefined);
+		return fbb.asUint8Array();
+	}
+
+	let ws: WebSocket | null = null;
+
+	function nextMessage(socket: WebSocket): Promise<MessageEvent<ArrayBuffer>> {
+		return new Promise((resolve, reject) => {
+			socket.addEventListener('close', reject);
+			const listener = (msg: MessageEvent<any>) => {
+				if (!(msg.data instanceof ArrayBuffer)) {
+					// ignore text frames
+					return;
+				}
+				socket.removeEventListener('close', reject);
+				socket.removeEventListener('message', listener);
+				resolve(msg);
+			};
+			socket.addEventListener('message', listener);
+		});
+	}
+
+	function connect(detail: any) {
+		const msg = identifyMsg(detail);
+		const wsUrl = env.PUBLIC_WS_URL;
+		if (!wsUrl) throw new Error('misconfigured');
+		ws = new WebSocket(wsUrl);
+		ws.binaryType = 'arraybuffer';
+		ws.onclose = () => {
+			ws = null;
+			throw new Error('disconnected');
+		};
+		ws.onopen = async () => {
+			ws!.send(msg);
+			const resp = await nextMessage(ws!);
+			const bb = new flatbuffers.ByteBuffer(new Uint8Array(resp.data));
+			const smsg = SmsgTable.getRootAsSmsgTable(bb);
+			(window as any)._SMSG = smsg;
+			console.log({ smsg });
+			(window as any)._smg = smsg.msg(new GameCreationResponseS());
+			console.log({ v: window._smg });
+		};
+	}
 </script>
 
 <Center>
@@ -22,6 +95,12 @@
 			}}
 		/>
 	{:else if stage == Stage.RoomType}
-		<RoomType {name} />
+		<RoomType
+			{name}
+			on:next={(evt) => {
+				stage = Stage.Connecting;
+				connect(evt.detail);
+			}}
+		/>
 	{/if}
 </Center>
--- frontend-svelte/src/routes/RoomType.svelte
+++ frontend-svelte/src/routes/RoomType.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	import Button from '../lib/Button.svelte';
 
+	const dispatch = createEventDispatcher();
+
 	export let name: string;
+	let pin: number;
 </script>
 
 <div>
@@ -9,12 +13,14 @@
 	<div>
 		<h4>Existing Game</h4>
 		<div class="flex w-full">
-			<input type="number" name="pin" class="text-3xl mr-1 flex-grow" size="10" />
-			<Button>JOIN</Button>
+			<input type="number" name="pin" class="text-3xl mr-1 flex-grow" size="10" bind:value={pin} />
+			<Button on:click={() => dispatch('next', { type: 'join', pin })}>JOIN</Button>
 		</div>
 	</div>
 	<div class="block">
 		<h4>New Game</h4>
-		<Button class="float-right">Create Game</Button>
+		<Button class="float-right" on:click={() => dispatch('next', { type: 'create' })}
+			>Create Game</Button
+		>
 	</div>
 </div>
