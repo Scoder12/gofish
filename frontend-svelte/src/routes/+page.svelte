8d0dc88be8d0ede357a83d91a5e6829720f61374
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-03-03 22:12:10 -0800

    WIP capnp-ts?

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
 	import Center from '../lib/Center.svelte';
-	import * as flatbuffers from 'flatbuffers';
 
 	import Name from './Name.svelte';
 	import RoomType from './RoomType.svelte';
-	import {
-		Cmsg,
-		CmsgTable,
-		Create,
-		ErrorS,
-		GameCreationResponseS,
-		GameRef,
-		IdentifyC,
-		Join,
-		Smsg,
-		SmsgTable
-	} from '../go-fish';
 	import { env } from '$env/dynamic/public';
+	import { Msg as C2SMsg, GameRef } from '../c2s.capnp';
 
 	enum Stage {
 		Name,
@@ -26,26 +14,6 @@
 
 	let stage = Stage.Name;
 	let name = '';
-
-	function gameFromDetails(fbb: flatbuffers.Builder, detail: any): [GameRef, number] {
-		if (detail.type == 'create') {
-			return [GameRef.Create, Create.createCreate(fbb)];
-		}
-		if (detail.type == 'join') {
-			return [GameRef.Join, Join.createJoin(fbb, detail.pin)];
-		}
-		throw new Error('Invalid game type');
-	}
-
-	function identifyMsg(detail: any): Uint8Array {
-		const fbb = new flatbuffers.Builder(1);
-		const nameOffset = fbb.createString(name);
-		const [gameType, gameOffset] = gameFromDetails(fbb, detail);
-		const identifyOffset = IdentifyC.createIdentifyC(fbb, nameOffset, gameType, gameOffset);
-		const msgTableOffset = CmsgTable.createCmsgTable(fbb, Cmsg.IdentifyC, identifyOffset);
-		fbb.finish(msgTableOffset, undefined);
-		return fbb.asUint8Array();
-	}
 
 	let ws: WebSocket | null = null;
 
@@ -65,21 +33,9 @@
 		});
 	}
 
-	function unpackMessage<T>(msg: ArrayBuffer, typ: Smsg, obj: T): T {
-		const bb = new flatbuffers.ByteBuffer(new Uint8Array(msg));
-		const smsg = SmsgTable.getRootAsSmsgTable(bb);
-		if (smsg.msgType() == Smsg.ErrorS) {
-			const error: ErrorS = smsg.msg(new ErrorS());
-			throw new Error(`Error from server: ${error.error()}`);
-		}
-		if (smsg.msgType() != typ) {
-			throw new Error(`Unexpected message type ${smsg.msgType()}, expected ${typ}`);
-		}
-		return smsg.msg(obj);
-	}
-
 	function connect(detail: any) {
-		const msg = identifyMsg(detail);
+		const m = new C2SMsg();
+		const msg = 'abcd';
 		const wsUrl = env.PUBLIC_WS_URL;
 		if (!wsUrl) throw new Error('misconfigured');
 		ws = new WebSocket(wsUrl);
@@ -91,8 +47,6 @@
 		ws.onopen = async () => {
 			ws!.send(msg);
 			const resp = await nextMessage(ws!);
-			const r = unpackMessage(resp.data, Smsg.GameCreationResponseS, new GameCreationResponseS());
-			console.log(r.id());
 		};
 	}
 </script>
